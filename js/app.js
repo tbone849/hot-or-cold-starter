de616dad10913b507b789da68a7eabb883634e57
Tim Sarvey <timsarvey@gmail.com>
2015-09-15 14:12:01 -0400

    Add function to get value from input

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 
 $(document).ready(function(){
+
+	getNumber();
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -15,3 +17,34 @@ $(document).ready(function(){
 });
 
 
+function newGame(){
+
+	// generate number between 1 and 100
+	var randomNumber = Math.floor((Math.random() * 10) + 1);
+
+
+}
+
+// get number from user
+function getNumber(){
+	$('#guessButton').click(function(){
+		var dataEntered = $('#userGuess').val(); // get value from textbox
+		var isNumber = parseInt(dataEntered); // parse answer to integer
+		console.log(isNumber)
+	});
+	
+/*
+	if(isNumber){ // is it a number?
+		if(isNumber < 15){
+			alert("I thought I said a number greater than 15...");
+			getNumber();
+		} else {
+			fizzbuzz(isNumber); // yes, run fizzbuzz
+		}
+		
+	} else {
+		alert("You did not enter a number. Try again."); // no, try again
+		getNumber(); // reprompt for a number
+	} */
+
+}
