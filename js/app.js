73e8b1f391e7b7e090878e9c1d43e9488dc5c77a
Tim Sarvey <timsarvey@gmail.com>
2015-09-16 17:36:20 -0400

    Added hot or cold statements based on guess

--- js/app.js
+++ js/app.js
@@ -14,6 +14,24 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+    // Handle form submission
+  function onFormSubmitted (event) {
+    event.preventDefault();
+    var guessedNumber = getNumber();
+    $('#userGuess').val(''); // empty textbox
+    if(isNaN(guessedNumber)) {
+      $("#feedback").text("You did not enter a number.\nPlease try again."); // no, try again
+    } else {
+      if(guessedNumber < 1 || guessedNumber > 100) { 
+        $("#feedback").text("You SHOULD enter a number between 1 and 100."); // no, try again
+      } else {
+        $('.guessBox').append("<li>" + guessedNumber + "</li>"); // it is a number between 1 and 100
+        counter();
+        compareNumbers(randomNewNumber, guessedNumber);
+      }
+    }
+  }
+
 });
 
 
@@ -21,23 +39,7 @@ function newGame(){
 
 }
 
-// Handle form submission
-function onFormSubmitted (event) {
-  event.preventDefault();
-  var guessedNumber = getNumber();
-  $('#userGuess').val(''); // empty textbox
-  if(isNaN(guessedNumber)) {
-    $("#feedback").text("You did not enter a number.\nPlease try again."); // no, try again
-  } else {
-    if(guessedNumber < 1 || guessedNumber > 100) { 
-      $("#feedback").text("You SHOULD enter a number between 1 and 100."); // no, try again
-    } else {
-      $('.guessBox').append("<li>" + guessedNumber + "</li>"); // it is a number between 1 and 100
-      counter();
-      // compareNumbers goes here
-    }
-  }
-}
+
 
 // get number from user
 function getNumber(){
@@ -52,12 +54,32 @@ function randomizeNumber(){
 }
 
 function compareNumbers(random, guessed){
+  console.log(random);
+  console.log(guessed);
+  var difference = random - guessed;
+  var absoluteDif = Math.abs(difference);
   if(random == guessed){
-    $('#feedback').text("You guessed correctly! Way to be awesome");
+    $('#feedback').text("You guessed correctly!\nWay to be awesome!");
   } 
+  else if(absoluteDif <= 10){
+    $('#feedback').text("You are burning hot!");
+  }
+  else if(absoluteDif <= 20){
+    $('#feedback').text("You are hot!");
+  }
+  else if(absoluteDif <= 30){
+    $('#feedback').text("You are warm.");
+  }
+  else if(absoluteDif <= 50){
+    $('#feedback').text("You are cold");
+  }
+  else {
+    $('#feedback').text("You are freezing");
+  }
  
 }
 
+// show guess count
 function counter(){
   var count = parseInt(($('#count').text()));
   count++;
